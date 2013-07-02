ca2da16961d4c83edc7e85d57cf8d2bfa497501f
Timothy Dang <tim@hooroo.com>
2013-07-02 16:16:33 +1000

    Rails 4 for the new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IssueWall::Application.config.secret_key_base = 'eebf109da2cf45726d95e5390ecf78c4b13cee12fab7fa00f599716df38b2460efcb77f991dca4383c25669b8d9c8b38ad7aefd59e74dba33baf8ae22b1a742d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IssueWall::Application.config.session_store :cookie_store, key: '_issue-wall_session'
